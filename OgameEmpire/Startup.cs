0a437e9ace95da656f95314703306eac43b1ca49
Reiss Florian <85802@supinfo.com>
2019-11-30 18:30:13 +0100

    Added swagger config

--- OgameEmpire/Startup.cs
+++ OgameEmpire/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace OgameEmpire
 {
@@ -24,12 +25,22 @@ namespace OgameEmpire
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ogame empire API --- V1");
+            });
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ogame empire API", Version = "v1" });
+            });
         }
     }
 }
